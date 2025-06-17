edb3fad82788383e8c6ca70a6bbbcc7ace8be54b
debut123 <160791890+debut123@users.noreply.github.com>
2025-06-17 09:50:57 +0200

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+    
+    <p>ceci est un test : <a href="test.php">Se connecter</a>
+
+
+
+</p>
+
+<a href="occurence.php">Test de connection database</a>
+
+<?php
+print_r(PDO::getAvailableDrivers());
+?>
+
+</body>
+</html>
